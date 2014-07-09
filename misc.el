532067d0eaf8121803f59671d2051f284c198b80
Kei Usui <icrossj@gmail.com>
2014-07-10 07:50:01 +0900

    misc update

--- misc.el
+++ misc.el
@@ -6,3 +6,8 @@
 
 ;;; 最近開いたファイルを保存する数を増やす
 (setq recentf-max-saved-items 10000)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(require 'volatile-highlights)
+(volatile-highlights-mode t)
