5d4dd20399fdaf3f25fbf2f26f8ef27cabde8d08
Kei Usui <icrossj@gmail.com>
2014-07-23 22:07:53 +0900

    helm ag tuning

--- init-helm.el
+++ init-helm.el
@@ -16,7 +16,7 @@
 
 ;;; helm ag
 (require 'helm-ag)
-(setq helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
+(setq helm-ag-base-command "ag --nocolor --nogroup --i")
 (setq helm-ag-insert-at-point 'symbol)
 (setq helm-ag-source-type 'file-line)
 
@@ -28,8 +28,8 @@
                          "-S -U -g")
  ;; 不安定な場合以下の項目を調整すれば
  ;; よくなるかもしれません
- ;; helm-ag-r-requires-pattern 3    ; 文字数以上入力してから検索
- ;; helm-ag-r-input-idle-delay 0.5  ; 検索をdelay後からおこなう
+ helm-ag-r-requires-pattern 3    ; 文字数以上入力してから検索
+ helm-ag-r-input-idle-delay 0.5  ; 検索をdelay後からおこなう
  ;; helm-ag-r-use-no-highlight t    ; ハイライト無効化
  helm-ag-r-candidate-limit 1000  ; 候補の上限を設定
  )
