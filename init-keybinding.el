dde5d9e2c80170e043362d47421702c93dca29da
Kei Usui <icrossj@gmail.com>
2014-07-25 02:10:17 +0900

    lots of keybind changes

--- init-keybinding.el
+++ init-keybinding.el
@@ -9,45 +9,50 @@
 (global-set-key (kbd "C-c i")   'helm-imenu)
 (global-set-key (kbd "C-x b")   'helm-buffers-list)
 
+;; helm-gtags
+(global-set-key [f1] 'helm-gtags-find-tag)
+(global-set-key [C-f1] 'helm-gtags-find-rtag)
+(global-set-key [C-f1] 'helm-gtags-find-symbol)
+
+;; git gutter
 (global-set-key [f2]    'git-gutter:next-hunk)
 (global-set-key [S-f2]  'git-gutter:previous-hunk)
 
+;; align
 (global-set-key [f4] 'align-current)
 (global-set-key [S-f4] 'align-entire)
 (global-set-key [C-f4] 'align-regexp)
 
-(global-set-key [f10] 'next-error)
-(global-set-key [S-f10] 'previous-error)
+;; line operation
+(global-set-key [f5] 'global-linum-mode)
+(global-set-key [C-f5] 'global-hl-line-mode)
 
-;; build operation
-(setq compilation-ask-about-save nil)
-(global-set-key [f9] 'recompile)
-(global-set-key [S-f9] 'compile)
+;; search operation
+(global-set-key [f6]   'helm-ag)
+(global-set-key [C-f6] 'helm-ag-r-from-git-repo)
+(global-set-key [S-f6] 'rgrep)
+(global-set-key [M-f6] 'grep-buffers)
+
+;; find file operation
+(global-set-key [f7] 'helm-for-files)
+(global-set-key [C-f7] 'helm-find-files)
+(global-set-key [S-f7] 'my-helm-git-files)
 
 ;; window operation
 (global-set-key [f8] 'other-window-or-split-horizontally)
 (global-set-key [C-f8] 'split-window-vertically)
 (global-set-key [S-f8] 'delete-other-windows)
 
-;; find file operation
-(global-set-key [f7] 'helm-recentf)
-(global-set-key [C-f7] 'my-helm-git-files)
+;; build operation
+(setq compilation-ask-about-save nil)
+(global-set-key [f9] 'recompile)
+(global-set-key [S-f9] 'compile)
 
-;; search operation
-(global-set-key [f6]   'helm-ag)
-(global-set-key [C-f6] 'helm-ag-r-from-git-repo)
-(global-set-key [S-f6] 'rgrep)
-
-;; line operation
-(global-set-key [f5] 'global-linum-mode)
-(global-set-key [C-f5] 'global-hl-line-mode)
+;; navigation
+(global-set-key [f10] 'next-error)
+(global-set-key [S-f10] 'previous-error)
 
 (require 'wdired)
 (define-key dired-mode-map (kbd "C-x C-q") 'wdired-change-to-wdired-mode)
-
-;; helm-gtags
-(global-set-key [f1] 'helm-gtags-find-tag)
-(global-set-key [C-f1] 'helm-gtags-find-rtag)
-(global-set-key [C-f1] 'helm-gtags-find-symbol)
 
 (global-unset-key (kbd "C-\\")) ; unbind toggle-input-method
