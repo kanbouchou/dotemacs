35a22e3d33b0aecbc47a66143760c20f1cfe3c37
Kei Usui <icrossj@gmail.com>
2015-08-17 22:50:10 +0900

    update keybind

--- init-diminish.el
+++ init-diminish.el
@@ -2,5 +2,6 @@
 (require 'diminish)
 (eval-after-load "anzu" '(diminish 'anzu-mode))
 (eval-after-load "git-gutter-fringe+" '(diminish 'git-gutter+-mode))
+(eval-after-load "smartparens" '(diminish 'smartparens-mode))
 (eval-after-load "volatile-highlights" '(diminish 'volatile-highlights-mode))
 (eval-after-load "auto-highlight-symbol" '(diminish 'auto-highlight-symbol-mode))
--- init-keybinding.el
+++ init-keybinding.el
@@ -12,9 +12,12 @@
 (global-set-key (kbd "C-.")  'helm-git-grep-at-point)
 (global-set-key (kbd "C-,")  'helm-multi-swoop-all)
 (global-set-key (kbd "C-o")  'helm-find-files) ; overwrite open-line
-
+(global-set-key (kbd "C-h")  'delete-backward-char); over
 (global-set-key (kbd "C-^")  'helm-resume)
 (global-set-key (kbd "C-z")  'helm-occur-from-isearch) ;
+
+(global-set-key (kbd "C-M-n") 'next-multiframe-window)
+(global-set-key (kbd "C-M-p") 'previous-multiframe-window)
 
 ;; window operation
 (global-set-key (kbd "C-\+") 'other-window-or-split)
@@ -26,52 +29,47 @@
 (global-set-key (kbd "<muhenkan>")  (lambda () (interactive) (set-input-method)))
 (global-set-key (kbd "<henkan>")  (lambda () (interactive) (set-input-method 'japanese-mozc)))
 
-;; line navigation
-(global-set-key [M-up]       'beginning-of-buffer)
-(global-set-key [M-down]     'end-of-buffer)
+;; navigation
+(global-set-key [M-up]   'beginning-of-buffer)
+(global-set-key [M-down] 'end-of-buffer)
 
-;; flyspell
-(global-set-key [C-f1]       'flyspell-buffer)
-(global-set-key [S-f1]       'flyspell-mode)
-(global-set-key [f1]         'flyspell-goto-next-error)
+;; git-gutter
+(global-set-key [f1]     'git-gutter+-next-hunk)
+(global-set-key [S-f1]   'git-gutter+-previous-hunk)
+(global-set-key [f2]     'git-gutter+-show-hunk)
+(global-set-key [C-f2]   'git-gutter+-revert-hunk)
 
-;; git
-(global-set-key [f2]         'magit-status)
-(global-set-key [C-f2]       'git-gutter+-revert-hunk)
-
-;; beautifulizer
-;; (load "/usr/share/emacs/site-lisp/clang-format-3.4/clang-format.el")
-;; (global-set-key [f3]         'clang-format-region)
+;; magit
+(global-set-key [f3]     'magit-status)
+(global-set-key [C-f3]   'magit-diff)
 
 ;; align
-(global-set-key [f4]         'align-current)
-(global-set-key [C-f4]       'align-regexp)
+(global-set-key [f4]     'align-current)
+(global-set-key [C-f4]   'align-regexp)
 
-(global-set-key [f5]         'helm-imenu)
+(global-set-key [f5]     'helm-imenu)
 
 ;; tags
-(global-set-key [f7]         'tags-pop-stack)
-(global-set-key [C-f7]       'rtags-location-stack-forward)
-(global-set-key [S-f7]       'tags-clear-stack)
-(global-set-key [f8]         'tags-find-symbol-at-point)
-(global-set-key [C-f8]       'tags-find-references-at-point)
+(global-set-key [f7]     'tags-pop-stack)
+(global-set-key [C-f7]   'rtags-location-stack-forward)
+(global-set-key [S-f7]   'tags-clear-stack)
+(global-set-key [f8]     'tags-find-symbol-at-point)
+(global-set-key [C-f8]   'tags-find-references-at-point)
 
 ;; build operation
 (setq compilation-ask-about-save nil)
-(global-set-key [f9]         'recompile)
-(global-set-key [C-f9]       'kill-compilation)
-(global-set-key [S-f9]       'compile)
+(global-set-key [f9]     'recompile)
+(global-set-key [C-f9]   'kill-compilation)
+(global-set-key [S-f9]   'compile)
 
 ;; navigation: error
-(global-set-key [f10]        'next-error)
-(global-set-key [S-f10]      'previous-error)
-
+(global-set-key [f10]    'next-error)
+(global-set-key [S-f10]  'previous-error)
 
 ;; navigation: rtags diagnostics error
-(global-set-key [f11]        'rtags-next-diag)
-(global-set-key [S-f11]      'rtags-previous-diag)
+(global-set-key [f11]    'rtags-next-diag)
+(global-set-key [S-f11]  'rtags-previous-diag)
 
-;; navigation: git
-(global-set-key [C-f12]      'global-git-gutter+-mode)
-(global-set-key [f12]        'git-gutter+-next-hunk)
-(global-set-key [S-f12]      'git-gutter+-previous-hunk)
+(require                 'expand-region)
+(global-set-key [f12]    'er/expand-region)
+(global-set-key [S-f12]  'er/contract-region)
