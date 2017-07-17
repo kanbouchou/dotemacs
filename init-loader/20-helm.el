29afe68418f88d152dceb30d1ae86e1bba1055ad
Kei Usui <kei.usui@mujin.co.jp>
2017-07-17 10:23:24 +0900

    change default locate command, doesn't work on wheezy's mlocate 0.23

--- init-loader/20-helm.el
+++ init-loader/20-helm.el
@@ -43,6 +43,9 @@
                                       (helm-ls-git-root-dir)))
              :keymap helm-ls-git-buffer-map)))
 
+;;; mlocate 0.23.1 doesn't have -A option. so overwrite default provided by helm-locate.el
+(setq helm-locate-command "locate %s -e --regex %s")
+
 ;;; helm ag
 (require 'helm-ag)
 (setq helm-ag-base-command "ag --nocolor --nogroup -i")
