afd392c3d18325aa0aa0e7040f4c7eb72ade942b
Kei Usui <kei.usui@mujin.co.jp>
2017-08-15 20:58:24 +0900

    trying new fonts

--- init-loader/01-font.el
+++ init-loader/01-font.el
@@ -1,6 +1,21 @@
-(set-default-font "DejaVu Sans Mono-11")
-(set-face-font 'variable-pitch "DejaVu Sans Mono-11")
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  '("Takaoゴシック" . "unicode-bmp")
-)
+(defun set-if-font-exists-p (myfont)
+  (when (member myfont (font-family-list))
+    (set-default-font myfont)
+    (set-face-attribute 'default nil
+                        :family myfont
+                        :height 110)
+    (set-fontset-font (frame-parameter nil 'font)
+                      'japanese-jisx0208
+                      (cons myfont "iso10646-1"))
+    (set-fontset-font (frame-parameter nil 'font)
+                      'japanese-jisx0212
+                      (cons myfont "iso10646-1"))
+    (set-fontset-font (frame-parameter nil 'font)
+                      'katakana-jisx0201
+                      (cons myfont "iso10646-1"))
+    (message "setting font %s" myfont)
+    )
+  )
+
+(set-if-font-exists-p "Ricty Diminished")
+(set-if-font-exists-p "Source Han Code JP")
