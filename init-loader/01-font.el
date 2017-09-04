069f3190ae0f45448b5d3b19897258248ce7fd40
Kei Usui <kei.usui@mujin.co.jp>
2017-09-05 08:43:05 +0900

    specify font instead of font family

--- init-loader/01-font.el
+++ init-loader/01-font.el
@@ -1,21 +1,28 @@
-(defun set-if-font-exists-p (myfont)
-  (when (member myfont (font-family-list))
-    (set-default-font myfont)
-    (set-face-attribute 'default nil
-                        :family myfont
-                        :height 110)
-    (set-fontset-font (frame-parameter nil 'font)
-                      'japanese-jisx0208
-                      (cons myfont "iso10646-1"))
-    (set-fontset-font (frame-parameter nil 'font)
-                      'japanese-jisx0212
-                      (cons myfont "iso10646-1"))
-    (set-fontset-font (frame-parameter nil 'font)
-                      'katakana-jisx0201
-                      (cons myfont "iso10646-1"))
-    (message "setting font %s" myfont)
+(defun set-font (myfont)
+  (set-face-attribute 'default nil
+                      :family myfont
+                      :height 110)
+  (set-fontset-font (frame-parameter nil 'font)
+                    'japanese-jisx0208
+                    (cons myfont "iso10646-1"))
+  (set-fontset-font (frame-parameter nil 'font)
+                    'japanese-jisx0212
+                    (cons myfont "iso10646-1"))
+  (set-fontset-font (frame-parameter nil 'font)
+                    'katakana-jisx0201
+                    (cons myfont "iso10646-1"))
+  (message "setting font %s" myfont)
+  )
+
+(defun font-existsp (font)
+    (if (null (x-list-fonts font))
+        nil t))
+
+(defun set-if-font-exists-p (myfont )
+  (if (font-existsp myfont)
+      (set-font myfont)
     )
   )
 
-(set-if-font-exists-p "Ricty Diminished")
-(set-if-font-exists-p "Source Han Code JP")
+;(set-if-font-exists-p "Ricty Diminished")
+(set-if-font-exists-p "Source Han Code JP R")
