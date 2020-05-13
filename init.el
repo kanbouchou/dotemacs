b25f42ea58a8549ddc1a618db005396aa7a34ef4
Kei Usui <icrossj@gmail.com>
2020-05-13 23:24:19 +0900

    c++ timer snippet

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (unless load-file-name
   (cd (getenv "HOME")))
 
@@ -10,5 +17,21 @@
 
 ;; init-loader
 (custom-set-variables
- '(init-loader-show-log-after-init 'error-only))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ag-highlight-search t)
+ '(ag-reuse-buffers (quote nil))
+ '(ag-reuse-window (quote nil))
+ '(cursor-type "hollow")
+ '(el-get-verbose t)
+ '(init-loader-show-log-after-init (quote error-only))
+ '(underline-minimum-offset 10))
 (init-loader-load (concat user-emacs-directory "init-loader"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight-indent-guides-character-face ((t (:foreground "#000043")))))
